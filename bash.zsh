7ec4d9e324d89589b5ffdae96280005063c295af
Seriki Walter Yakub <251400999+auraecosystem@users.noreply.github.com>
2026-08-15 00:50:50 +0100

    Update bash.zsh

--- bash.zsh
+++ bash.zsh
@@ -1,3 +1,7 @@
 man git-remote add origin git@github.com:auraecosystem/my-blog.git
 git branch -M main
 git push -u origin main
+cd my-blog
+git init
+git add .
+git commit -m "feat: initialize blog repository setup"
